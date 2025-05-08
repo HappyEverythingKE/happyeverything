52505c37151794814f86da3dc41527952400b69d
Rehema Wachira <20615801+remyroo@users.noreply.github.com>
2025-05-08 22:01:49 +0300

    Rewrite (#3)
    * delete current setup
    
    * Add tanstack router app and setup linting
    
    * add husky and lint-staged precommit hook
    
    * tweak eslint config and delete DS_Store
    
    * add dependabot config

---
+++ src/routes/__root.tsx
@@ -0,0 +1,23 @@
+import { createRootRouteWithContext, Outlet } from '@tanstack/react-router'
+import type { QueryClient } from '@tanstack/react-query'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+
+import Header from '../components/Header'
+import TanstackQueryLayout from '../integrations/tanstack-query/layout'
+
+interface MyRouterContext {
+  queryClient: QueryClient
+}
+
+export const Route = createRootRouteWithContext<MyRouterContext>()({
+  component: () => (
+    <>
+      <Header />
+
+      <Outlet />
+      <TanStackRouterDevtools />
+
+      <TanstackQueryLayout />
+    </>
+  ),
+})
