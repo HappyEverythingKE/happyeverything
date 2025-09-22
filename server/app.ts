693803e3e3ac60d98ae927a6b10ef2ab0ace011e
Rehema Wachira <20615801+remyroo@users.noreply.github.com>
2025-09-22 11:51:24 +0300

    fix patch error

--- server/app.ts
+++ server/app.ts
@@ -32,7 +32,10 @@ const apiRoutes = app
       c.header('Access-Control-Allow-Origin', origin)
     }
     c.header('Access-Control-Allow-Credentials', 'true')
-    c.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    c.header(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, PATCH, DELETE, OPTIONS',
+    )
     c.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
 
     if (c.req.method === 'OPTIONS') {
